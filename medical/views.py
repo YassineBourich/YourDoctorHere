14c4b729abc01262d9dce5b09d0500bb2192fa6d
SoufianeAitlhadj <aitlhadjsoufiane@gmail.com>
2026-03-14 17:49:03 +0000

    test commit

--- medical/views.py
+++ medical/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
