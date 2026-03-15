7d1a702abe7e2bcc2187ed259adddd5c564ebdca
YassineBourich <YassineBourich2006@gmail.com>
2026-03-15 15:35:01 +0000

    Creating user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required for healthcare accounts")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        
+        # This is the most important security step:
+        user.set_password(password) # Scrambles the password
+        
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class Entity(models.TextChoices):
+    PATIENT = 'Patient'
+    DOCTOR = 'Doctor'
+    HOSPITAL = 'Hospital'
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    entity = models.CharField(choices=Entity.choices, default=Entity.PATIENT)
+
+    # Tell Django that 'email' is the new ID for login
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['entity'] 
+
+    # Connect the Manager to this Model
+    objects = UserManager()
+
+class Patient(models.Model):
+    
--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('registration/', None, name=""),
+    path('login/', None, name=""),
+    path('email_verification/', None, name=""),
+    path('doctor_validation/', None, name=""),
+    path('patients_history/', None, name=""),
+    path('doctor_validation/', None, name=""),
+    path('doctors/', None, name=""),
+    path('doctors/<uuid: id>/', None, name=""),
+    path('doctors/<uuid: id>/slot', None, name=""),
+    path('patients/me/', None, name=""),
+    path('patients/me/history/', None, name=""),
+    path('patients/<uuid: id>/', None, name=""),
+    path('patients/<uuid: id>/history/', None, name=""),
+    path('hospitals/', None, name=""),
+    path('hospitals/<uuid: id>/', None, name=""),
+    path('hospitals/<uuid: id>/doctors/', None, name=""),
 ]
