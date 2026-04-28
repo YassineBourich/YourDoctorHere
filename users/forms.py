c9372f6b2839a684c284e605d5dc43f1f1990742
YassineBourich <YassineBourich2006@gmail.com>
2026-04-28 13:03:45 +0100

    Adding password validation at the level of registration

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,35 @@
 from django import forms
 from .models import User, Patient, Doctor
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['email', 'password', 'confirm_password']
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        try:
+            validate_password(password, self.instance)
+        except ValidationError as e:
+            raise forms.ValidationError(list(e.messages))
+        return password
+
+    def clean(self):
+        # This function checks if the two passwords match
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            # If they don't match, we add an error to the 'password' field
+            self.add_error('confirm_password', "Passwords do not match.")
+        
+        return cleaned_data
 
 class PatientForm(forms.ModelForm):
     class Meta:
